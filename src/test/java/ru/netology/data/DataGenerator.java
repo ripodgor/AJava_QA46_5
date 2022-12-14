52edc65d561c76aa74fd33d39c891869a3b05f93
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-12-14 16:34:59 +0300

    Updated files

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,11 +1,10 @@
 package ru.netology.data;
 
 import com.github.javafaker.Faker;
+import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 
@@ -13,13 +12,12 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-    public static String generateDate(int days) {
-        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        return date;
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     public static String generateCity() {
-        List<String> list = Arrays.asList("Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик",
+        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик",
                 "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск", "Якутск",
                 "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита",
                 "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь", "Владивосток", "Ставрополь",
@@ -29,19 +27,33 @@ public class DataGenerator {
                 "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза", "Псков", "Ростов-на-Дону",
                 "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург", "Смоленск", "Тамбов", "Тверь",
                 "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск", "Ярославль", "Москва", "Санкт-Петербург",
-                "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард");
-        Random rand = new Random();
-        String city = list.get(rand.nextInt(list.size()));
-        return city;
+                "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"};
+        return cities[new Random().nextInt(cities.length)];
     }
 
-    public static String generateName() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.name().firstName() + " " + faker.name().lastName();
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
     }
 
-    public static String generatePhone() {
-        Faker faker = new Faker(new Locale("ru"));
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
     }
 }
--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,18 +1,21 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class CardDeliveryTest {
+class CardDeliveryTest {
 
     @BeforeEach
     void setup() {
@@ -20,240 +23,34 @@ public class CardDeliveryTest {
     }
 
     @Test
-    void shouldSendForm() {
-        String planningDate = DataGenerator.generateDate(7);
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldAcceptYoInCity() {
-        String planningDate = DataGenerator.generateDate(7);
-        $("[data-test-id='city'] input").val("Орёл");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldAcceptDash() {
-        String planningDate = DataGenerator.generateDate(7);
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
-        $("[data-test-id='name'] input").val("Петрова-Водкина Анна-Мария");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldSendWithoutPlusInPhone() {
-        String planningDate = DataGenerator.generateDate(7);
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val("79991112233");
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldResendFormWithOtherDate() {
-        String city = DataGenerator.generateCity();
-        String name = DataGenerator.generateName();
-        String phone = DataGenerator.generatePhone();
-        String planningDate = DataGenerator.generateDate(7);
-        String secondPlanningDate = DataGenerator.generateDate(5);
-        $("[data-test-id='city'] input").val(city);
-        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
-        $("[data-test-id='name'] input").val(name);
-        $("[data-test-id='phone'] input").val(phone);
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").val(city);
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(5));
-        $("[data-test-id='name'] input").val(name);
-        $("[data-test-id='phone'] input").val(phone);
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
+    @DisplayName("Should successful plan meeting")
+    void shouldSuccessfulPlanMeeting() {
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysToAddForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        int daysToAddForSecondMeeting = 7;
+        String secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $(byText("Запланировать")).click();
+        $("[data-test-id=replan-notification]").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Необходимо подтверждение" +
+                        " У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
         $("[data-test-id='replan-notification'] button").click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + secondPlanningDate), Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldNotSendEmptyByDefault() {
-        $(withText("Запланировать")).click();
-        $("[data-test-id='city'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldNotSendWithoutDate() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='date'] .input__sub").shouldHave(text("Неверно введена дата"));
-    }
-
-    @Test
-    void shouldNotSendWithoutName() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldNotSendWithoutPhone() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldNotSendWithoutAgreement() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $(withText("Запланировать")).click();
-        $("[data-test-id='agreement'].input_invalid").should(appear);
-    }
-
-    @Test
-    void shouldNotSendWithoutSurname() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val("Иван");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldNotSendWithSpaceForName() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val(" ");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldNotSendWithDashForName() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val("-");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldNotSendCityOutOfList() {
-        $("[data-test-id='city'] input").val("Королев");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='city'].input_invalid .input__sub").shouldHave(text(
-                "Доставка в выбранный город недоступна"));
-    }
-
-    @Test
-    void shouldNotAcceptDateInPast() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(-7));
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='date'] .input__sub").shouldHave(text(
-                "Заказ на выбранную дату невозможен"));
-    }
-
-    @Test
-    void shouldNotSendLatinName() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val("Ivanov Ivan");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text(
-                "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    void shouldNotSendSpecialSymbolsForName() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val("!#&?");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text(
-                "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    void shouldNotSendNumbersInName() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val("Иван 1");
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text(
-                "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    void shouldNotSendLessDigitsInPhone() {
-        $("[data-test-id='city'] input").val(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val("+799911122");
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(text(
-                "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void shouldUseCityList() {
-        String planningDate = DataGenerator.generateDate(7);
-        $("[data-test-id='city'] input").val("мо");
-        $$(".menu-item__control").findBy(text("Москва")).click();
-        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
-        $("[data-test-id='name'] input").val(DataGenerator.generateName());
-        $("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Запланировать")).click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Успешно! Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(visible);
     }
 }
